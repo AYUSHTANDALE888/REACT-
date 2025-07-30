92d02f3885693041722690b278b39b59795dd0b4
Ayush Tandale <ayushtandale666@gmail.com>
2025-07-30 13:46:59 +0530

    episode-2

--- EP-1/App.js
+++ EP-1/App.js
@@ -1,22 +1,10 @@
-/*
-* <div id="parent">
-*     <div id="child1">
-*          <h1>I'm an h1 tag</h1>
-*          <h2>I'm an h2 tag</h2>
-*     </div>
-*     <div id="child2">
-*          <h1>I'm an h1 tag</h1>
-*          <h2>I'm an h2 tag</h2>
-*     </div>
-* </div>
-*
-* ReactElement(Object) => HTML(Browser Understand)
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", 
     {id: "parent"}, 
     [React.createElement("div", {id: "child1"},
-        [React.createElement("h1", {}, "I'm an h1 tag"), 
+        [React.createElement("h1", {}, "This is Namaste React🚀"), 
             React.createElement("h2", {}, "I'm an h2 tag")
         ]),
     React.createElement("div", {id: "child2"},
@@ -24,13 +12,6 @@ const parent = React.createElement("div",
             React.createElement("h2", {}, "I'm an h2 tag")
         ])
 ])
-
-//JSX
-
-// const heading = React.createElement("h1", 
-//     {id: "heading", xyz: "abc"}, 
-//     "Hello World from React!"
-// );
 
 console.log(parent); //Object
 
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # NAMASTE-REACT
+
+
+# parcel 
+- Dev build
+- local server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Diagnostics
+- Error Handling
+- Tree Shaking - remove unused code 
+- HTTPs
+- Code Splitting
+- Differential Bundling - support older browsers 
+- Different dev and prod bundles 
