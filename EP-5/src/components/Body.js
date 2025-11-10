5e8abc2362d673cc150d70ebcddf2da1451dbd60
Ayush Tandale <ayushtandale666@gmail.com>
2025-11-10 23:40:29 +0530

    EP-5

--- EP-5/src/components/Body.js
+++ EP-5/src/components/Body.js
@@ -1,11 +1,28 @@
 import Restaurantcards from "./Restaurantcards";
-import resList from "../utils/mockData";
+import { useState } from "react";
+import reslist from "../utils/mockData";
 
 const Body = () => {
+
+    // Local State variable - super powerful variable
+
+    const [listOfRestaurants, setlistOfRestaurants] = useState(reslist);
+
     return (
         <div className="body">
-            <div className="search">Search</div>
-            <div className="resto-container">
+            <div className="filter">
+                <button className="filter-btn"
+                onClick = { () => {
+                    // Filter logic
+                    const FilteredList = listOfRestaurants.filter( 
+                        (res) => res.data.avgRating > 4
+                    );
+                    setlistOfRestaurants(FilteredList);
+                }}>
+                    TOP RATED RESTAURANTS
+                </button>
+            </div>
+            <div className="resto-container">   
                 {/* <Restaurantcards resData={resList[0]} />
                 <Restaurantcards resData={resList[1]} />
                 <Restaurantcards resData={resList[2]} />
@@ -15,7 +32,7 @@ const Body = () => {
                 <Restaurantcards resData={resList[6]} />
                 <Restaurantcards resData={resList[7]} />
                 <Restaurantcards resData={resList[8]} /> */}
-                {resList.map((restaurant) => (
+                {listOfRestaurants.map((restaurant) => (
                     <Restaurantcards key={restaurant.data.id} resData={restaurant} />
                 ))}
             </div>
